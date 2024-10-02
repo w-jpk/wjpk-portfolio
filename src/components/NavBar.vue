9a2f901f7363f9a701c63b2061fca5a7d700885e
w-jpk <gizbrekht.y@mail.ru>
2024-10-02 18:08:28 +0300

    fix navbar / create Experience component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,6 @@
     <div>
       <ul class="nav-links">
         <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
